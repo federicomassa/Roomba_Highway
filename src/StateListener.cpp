fcef18d69d26c9ad2dc66ce584e9b3dd37bb09f7
Federico Massa <fedemassa91@gmail.com>
2017-02-23 11:40:24 +0100

    test SensingVector

--- src/StateListener.cpp
+++ src/StateListener.cpp
@@ -4,23 +4,24 @@
  */
 
 #include "ros/ros.h"
-#include "irobotcreate2/State.h"
+#include "irobotcreate2/SensingVector.h"
 
 #include <string>
 #include <sstream>
 
-void StateReceived(const irobotcreate2::State& q)
+void StateReceived(const irobotcreate2::SensingVector& sv)
 {
-  std::stringstream ss;
   std::string msg;
 
-  ss << "x: " << q.x << '\t' << "y: " << q.y << '\t' << "theta: " << 
-    q.theta << '\t' << "v: " << q.v << '\t' << "desiredV: " << q.desiredV;
+  msg = std::to_string(sv.sList.size()) + " states.\n";
 
-  ss >> msg;
-  ss.clear();
+  for (int i = 0; i < sv.sList.size(); i++)
+    {
+      msg += "[ID: " << std::to_string(sv.sList[i].id) << "\tx: " + std::to_string(sv.sList[i].x) + "\ty: " + std::to_string(sv.sList[i].y) + "\ttheta: " + 
+	std::to_string(sv.sList[i].theta) + "\tv: " + std::to_string(sv.sList[i].v) + "]\n";
+    }
 
-  ROS_INFO("Received: [%s]", msg.c_str()); 
+  ROS_INFO("Received: %s", msg.c_str()); 
 }
 
 int main(int argc, char **argv)
